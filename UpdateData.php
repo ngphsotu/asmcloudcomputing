d1b76b2527e492b460d45f29f194e3f53a84a3ed
tungnps <tungnpsgch16064@fpt.edu.vn>
2019-07-07 17:01:23 +0700

    Update UpdateData.php

--- UpdateData.php
+++ UpdateData.php
@@ -1,42 +1,83 @@
 <!DOCTYPE html>
 <html>
-  <body>
-    <h1>UPDATE DATA TO DATABASE</h1>
-    <ul>
-      <form name="UpdateData" action="UpdateData.php" method="POST" >
-        <li style="list-style: none;">User ID:</li><li style="list-style: none;"><input type="text" name="stuid" /></li>
-        <li style="list-style: none;">Full Name:</li><li style="list-style: none;"><input type="text" name="fname" /></li>
-        <li style="list-style: none;">Email:</li><li style="list-style: none;"><input type="text" name="email" /></li>
-        <li style="list-style: none;">Address:</li><li style="list-style: none;"><input type="text" name="classname" /></li>
-        <li style="list-style: none;"><input type="submit" /></li>
-      </form>
-    </ul>
-    <?php
-    // ini_set('display_errors', 1);
-    // echo "Update database!";
-    ?>
-    <?php
-      if (empty(getenv("DATABASE_URL"))){
-          echo '<p>The DB does not exist</p>';
-          $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
-      }  else {
-         $db = parse_url(getenv("DATABASE_URL"));
-         $pdo = new PDO("pgsql:" . sprintf(
-             "host=ec2-107-20-173-2.compute-1.amazonaws.com;port=5432;user=epjhznfybiofsdp;password=d710f945ed11c0ee09b1a7c5760479da13c43a2c7d58694bd38d4075d1f0c453;dbname=dg80gb8rdsb12",
-              $db["host"],
-              $db["port"],
-              $db["user"],
-              $db["pass"],
-              ltrim($db["path"], "/")
-         ));
-      }  
-      $sql = "UPDATE student SET fname = '$_POST[fname]', email = '$_POST[email]', classname = '$_POST[classname]' WHERE stuid = '$_POST[stuid]'";
+    <link rel="stylesheet" href="style.css">
+    <style>
+        li {
+            list-style: none;
+        }
+    </style>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script>
+        function CheckClass() {
+            var CheckClassName = document.getElementById("Class1").value;
+            var checkFullName = document.getElementById("Name1").value;
+            var checkEmail = document.getElementById("Email1").value;
+            if (CheckClassName == "GCD0819") {
+                return true;
+            } else if (checkFullName == "") {
+                alert("FullName should have Data");
+                return false;
+            } else if (checkEmail == "") {
+                alert("Email should have Data");
+                return false;
+            } else {
+                alert("ClassName should equal GCD0819");
+                return false;
+            }
+        }
+    </script>
+    <body>
+        <h1>Update to the database</h1>
+        <ul>
+            <form name="UpdateData" action="UpdateData.php" method="POST">
+                <li>Student ID:</li>
+                <li><input type="text" name="stuid" id= /></li>
+                <li>Full Name:</li>
+                <li><input type="text" name="fname" id="Name1" /></li>
+                <li>Email:</li>
+                <li><input type="text" name="email" id="Email1" /></li>
+                <li>Class:</li>
+                <li><input type="text" name="classname" id="Class1" /></li>
+                <li><input type="submit" onclick="CheckClass()" /></li>
+            </form>
+        </ul>
+        <div class="row">
+            <div class="col-12">
+                <a href="ConnectToDB.php" class="myButton pl-3">View Data</a>
+
+                <a href="InsertData.php" class="myButton pl-3">Insert data to the database</a>
+
+                <a href="DeleteData.php" class="myButton pl-3">Delete data to the database</a>
+            </div>
+        </div>
+        <?php
+        // ini_set('display_errors', 1);
+        // echo "Update database!";
+        ?>
+        <?php
+            if (empty(getenv("DATABASE_URL"))) {
+                echo '<p>The DB does not exist</p>';
+                $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
+            } else {
+
+                $db = parse_url(getenv("DATABASE_URL"));
+                $pdo = new PDO("pgsql:" . sprintf(
+                    "host=ec2-107-20-173-2.compute-1.amazonaws.com;port=5432;user=pjhznfybiofsdp;password=d710f945ed11c0ee09b1a7c5760479da13c43a2c7d58694bd38d4075d1f0c453;dbname=ddg80gb8rdsb12",
+                    $db["host"],
+                    $db["port"],
+                    $db["user"],
+                    $db["pass"],
+                    ltrim($db["path"], "/")
+                ));
+            }
+            $sql = "UPDATE student SET fname = '$_POST[fname]', email = '$_POST[email]', classname = '$_POST[classname]'
+                WHERE stuid = '$_POST[stuid]'";
             $stmt = $pdo->prepare($sql);
-      if($stmt->execute() == TRUE){
-          echo "Record updated successfully.";
-      } else {
-          echo "Error updating record. ";
-      }
-    ?>
-  </body>
+            if ($stmt->execute() == TRUE) {
+                echo "Record updated successfully.";
+            } else {
+                echo "Error updating record. ";
+            }
+        ?>
+    </body>
 </html>
